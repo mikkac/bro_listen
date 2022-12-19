4c423478d0127330f04bf5ca3a96e9fc92e3b9da
Mikolaj Kaczmarek <m.kaczmarek9@gmail.com>
2022-12-19 17:46:10 +0100

    updated main file

--- jarvis/jarvis.py
+++ jarvis/jarvis.py
@@ -1,108 +1,46 @@
 #!/usr/bin/env python3
 
 import argparse
-import json
-import queue
-import sys
-import sounddevice as sd
-import openai
 import os
+import sys
+
+from colored import fg, attr
 import pyttsx3
 
-from vosk import Model, KaldiRecognizer
+from voice import VoiceRecognizer, VoskVoiceRecognizer
+from chat import init_chat_api, request_chat_response
+from utils import get_args
 
-q = queue.Queue()
 
-def int_or_str(text):
-    """Helper function for argument parsing."""
+def main(args: argparse.Namespace) -> None:
+    init_chat_api(os.environ.get("OPENAI_API_KEY"))
+
+    voice_recognizer: VoiceRecognizer = VoskVoiceRecognizer(
+        language="en-us", device=args.device)
+
+    print('-' * 80)
+    print(f'{attr("bold")}Waiting for command.. {attr("reset")}')
+    print('-' * 80)
+    for prompt in voice_recognizer.listen():
+        print(f'{fg("green")}{attr("bold")}Prompt:{attr("reset")}\n{prompt}\n')
+
+        response = request_chat_response(prompt)
+
+        print(f'{fg("blue")}{attr("bold")}Response:{attr("reset")}\n{response}\n')
+
+        print('-' * 80)
+        print(f'{attr("bold")}Waiting for command...{attr("reset")}')
+        print('-' * 80)
+        # if audio:
+        # pyttsx3.speak(response)
+
+
+if __name__ == "__main__":
     try:
-        return int(text)
-    except ValueError:
-        return text
-
-def callback(indata, frames, time, status):
-    """This is called (from a separate thread) for each audio block."""
-    if status:
-        print(status, file=sys.stderr)
-    q.put(bytes(indata))
-
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument(
-    "-l", "--list-devices", action="store_true",
-    help="show list of audio devices and exit")
-args, remaining = parser.parse_known_args()
-if args.list_devices:
-    print(sd.query_devices())
-    parser.exit(0)
-parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    parents=[parser])
-parser.add_argument(
-    "-f", "--filename", type=str, metavar="FILENAME",
-    help="audio file to store recording to")
-parser.add_argument(
-    "-d", "--device", type=int_or_str,
-    help="input device (numeric ID or substring)")
-parser.add_argument(
-    "-a", "--audio", action='store_true',
-    help="Enable audio response")
-args = parser.parse_args(remaining)
-
-openai.api_key = os.environ.get("OPENAI_API_KEY")
-if openai.api_key is None:
-    print("To use copilot please set the OPENAI_API_KEY environment variable")
-    print("You can get an API key from https://beta.openai.com/account/api-keys")
-    print("To set the environment variable, run:")
-    print("export OPENAI_API_KEY=<your key>")
-    sys.exit(1)
-    
-def chat_request(prompt):
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=1024,
-        top_p=1,
-        stop=["`"],
-        frequency_penalty=0,
-        presence_penalty=0,
-        n=1,
-    )
-    response = strip_all_whitespaces_from(response.choices)[0]
-    return response
-
-def strip_all_whitespaces_from(choices):
-    return [choice.text.strip() for choice in choices]
-
-try:
-    audio = args.audio
-    device_info = sd.query_devices(args.device, "input")
-    samplerate = int(device_info["default_samplerate"])
-    model = Model(lang="en-us")
-
-    with sd.RawInputStream(samplerate=samplerate, blocksize = 8000, device=args.device,
-            dtype="int16", channels=1, callback=callback):
-        print("#" * 80)
-        print("Press Ctrl+C to stop the recording")
-        print("#" * 80)
-
-        rec = KaldiRecognizer(model, samplerate)
-        print("ok, please give me the command:")
-        while True:
-            data = q.get()
-            if rec.AcceptWaveform(data):
-                prompt = json.loads(rec.Result()).get("text", None)
-                if prompt:
-                    print(prompt)
-                    print("cool, here is chatgpt response:")
-                    response = chat_request(prompt)
-                    print(response)
-                    if audio:
-                        pyttsx3.speak(response)
-
-except KeyboardInterrupt:
-    print("\nDone")
-    parser.exit(0)
-except Exception as e:
-    parser.exit(type(e).__name__ + ": " + str(e))
+        main(get_args())
+    except KeyboardInterrupt:
+        print("\nDone")
+        sys.exit(0)
+    except Exception as e:
+        print(type(e).__name__ + ": " + str(e))
+        sys.exit(1)
